6141be6b3d1aa7dd6b208956ffd948178b6bb73f
maheshwararaj07 <maheshwararaj2003@gmail.com>
2024-05-20 00:05:21 +0530

    imported images for skill section

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -8,11 +8,13 @@ import resume from './resume.pdf'
 import project1 from './project-img/placeholder-image-1.png'
 import project2 from './project-img/placeholder-image-1.png'
 import project3 from './project-img/placeholder-image-1.png'
-import fronted from './project-img/fronted.png'
-import backend from './project-img/backend.png'
-import program from './project-img/program.png'
-import tag from './project-img/tag.png'
+import fronted from './frontend.png'
+import backend from './backend.png'
+import program from './program.png'
+import tag from './tag.png'
 const projects = [project1,project2,project3]
+const skills = [fronted,backend,program,tag]
+
 export const assets = {
     insta,
     linkedin,
@@ -21,5 +23,7 @@ export const assets = {
     myimg,
     background,
     resume,
-    projects
+    projects,
+    skills
+
 }   
